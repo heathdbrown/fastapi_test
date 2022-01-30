b438c442e74ecb1ca06cf3fd311199c74a1081b3
Heath Brown <heathd.brown@gmail.com>
2022-01-30 12:12:35 -0600

    Fixes syntax error with shutdown function

--- src/app/main.py
+++ src/app/main.py
@@ -16,7 +16,7 @@ async def startup():
     await database.connect()
 
 @app.on_event("shutdown")
-async def shutdown()
+async def shutdown():
     await database.disconnect()
 
 app.include_router(ping.router)
