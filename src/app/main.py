4452c86ed5040c2f90141c6623b9eca79cd19085
Heath Brown <heathd.brown@gmail.com>
2022-01-30 09:53:39 -0600

    Changes ping to async

--- src/app/main.py
+++ src/app/main.py
@@ -8,5 +8,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/ping")
-def pong():
+async def pong():
+    # some async  operation could happen here
+    # example: notes = await get_all_notes()
     return {"ping": "pong!"}
